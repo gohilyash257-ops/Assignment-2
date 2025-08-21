938afc819875b06a3599ed9e746ccac4eece1229
gohilyash257-ops <gohilyash257@gmail.com>
2025-08-21 23:23:29 +0530

    Add files via upload

---
+++ Task 3.py
@@ -0,0 +1,7 @@
+#check if the number is even or odd.
+a=int(input('Enter the number: '))
+
+if a%2==0:
+    print(a,'is an even number.')
+else:
+    print(a,'is an odd number.')
---
+++ Task 4.py
@@ -0,0 +1,7 @@
+#sum of integers from 1 to 50 using a loop.
+
+a=0
+for i in range(1,51):
+ a=a+i
+
+print("sum of the numbers 1 to 50 is:",a)
